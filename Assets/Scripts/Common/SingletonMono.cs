c3738152f887da60045ddf6c07bf24d3bd4bb7bb
duongxuanlong <xuanlong.8888@gmail.com>
2020-03-25 21:19:29 +0700

    add some common scripts

---
+++ Assets/Scripts/Common/SingletonMono.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SingletonMono<T> : MonoBehaviour where T : SingletonMono<T>
+{
+    private static T _Instance;
+
+    public T Instance 
+    {
+        get
+        {
+            if (_Instance == null)
+                _Instance = this as T;
+            return _Instance;
+        }
+    }
+
+    private void Awake() {
+        _Instance = this as T;
+    }
+}
