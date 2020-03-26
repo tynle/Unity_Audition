66ff9399337e56473b172d648be2bea6c0be02be
tuyen.lenhat <tuyen.lenhat@gameloft.com>
2020-03-26 18:56:32 +0700

    - show dancers

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -6,6 +6,15 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
     private StageManager stageScript;
+    private GAMESTATE currentState;
+    enum GAMESTATE
+    {
+        GS_INIT,
+        GS_CINEMATIC,
+        GS_PREPAIR,
+        GS_DANCE,
+        GS_STATS
+    }
     void Awake()
     {
         if(instance == null)
@@ -14,11 +23,39 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         DontDestroyOnLoad(gameObject);
         stageScript = GetComponent<StageManager>();
-        InitGame();
+        currentState = GAMESTATE.GS_INIT;
     }
 
     public void InitGame()
     {
         stageScript.SetupStage();
+    }
+
+    public void UpdateGame()
+    {
+        switch(currentState)
+        {
+            case GAMESTATE.GS_INIT:
+                InitGame();
+                currentState = GAMESTATE.GS_CINEMATIC;
+            break;
+            case GAMESTATE.GS_CINEMATIC:
+                currentState = GAMESTATE.GS_PREPAIR;
+            break;
+            case GAMESTATE.GS_PREPAIR:
+                stageScript.GenerateButtons(stageScript.buttons, 2, 6);
+                stageScript.LayoutButtons();
+                currentState = GAMESTATE.GS_DANCE;
+            break;
+            case GAMESTATE.GS_DANCE:
+            break;
+            case GAMESTATE.GS_STATS:
+            break;
+        }
+
+    }
+    void Update()
+    {
+        UpdateGame();
     }
 }
