3c768b39921c5b2a1a9b90f1d5fa5ee2440ee4c6
chanhvotrung <chanh.votrung@gmail.com>
2020-03-27 04:26:02 +0700

    +commit to UI

---
+++ Assets/Scripts/Common/Commands/DownCommand.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DownCommand : ICommand
+{
+    public void Excute()
+    {
+        // throw new System.NotImplementedException();
+        GameManager.Instance.ProcessCommand(PoolManager.PoolObject.Down);
+    }
+}
