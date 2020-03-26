fda7e4496660e55b9c161802d5689cbe52ff2033
tuyen.lenhat <tuyen.lenhat@gameloft.com>
2020-03-26 14:24:19 +0700

    - update scripts

---
+++ Assets/Scripts/Manager/StageManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StageManager : MonoBehaviour
+{
+    public GameObject canvas;
+    
+    public void SetupStage()
+    {
+        GameObject ui = Instantiate(canvas, new Vector3(0f,0f,0f), Quaternion.identity) as GameObject;
+        ui.transform.Find("bgImage").SetAsFirstSibling();
+        ui.transform.Find("Hub").SetAsLastSibling();
+    }
+    
+}
